6763f4af724c29ae4d622011b6641669544e6676
Ahmed Osama <ahmed.osama.abbas@gmail.com>
2017-08-22 04:09:02 +0200

    Create CF61-D2-D.cpp

---
+++ Problems Solutions/Codeforces/CF61-D2-D.cpp
@@ -0,0 +1,55 @@
+#pragma warning (disable : 4996 4018)
+
+//#include <thread>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <bitset>
+#include <limits>
+#include <iomanip>
+#include <sstream>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include <functional>
+using namespace std;
+
+int n;
+vector<pair<int, int>> vec[100000];
+vector<pair<int, long long>> leaf;
+
+void solve(int nxt, int par, long long cost) {
+	bool f = 0;
+	for (int i = 0; i < vec[nxt].size(); i++)
+		if (vec[nxt][i].first != par) {
+			f = 1;
+			solve(vec[nxt][i].first, nxt, cost + vec[nxt][i].second);
+		}
+	if (!f)
+		leaf.push_back({ nxt, cost });
+}
+
+int main() {
+	cin.tie(0);
+	ios_base::sync_with_stdio(0);
+	cin >> n;
+	long long ans = 0;
+	for (int i = 0; i < n - 1; i++) {
+		int a, b, c;
+		cin >> a >> b >> c;
+		a--, b--;
+		vec[a].push_back({ b, c });
+		vec[b].push_back({ a, c });
+		ans += c * 2;
+	}
+	solve(0, -1, 0);
+	long long mx = 0;
+	for (int i = 0; i < leaf.size(); i++)
+		mx = max(mx, leaf[i].second);
+	cout << ans - mx << '\n';
+	return 0;
+}
